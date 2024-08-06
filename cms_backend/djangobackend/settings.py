b07d7ffe86586e819ec17b19688edd39335a345c
hml263 <htetmyetlwin263@gmail.com>
2024-08-06 12:23:32 +0700

    updated config

--- cms_backend/djangobackend/settings.py
+++ cms_backend/djangobackend/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #bool(os.environ.get("DEBUG", default=0))
+DEBUG = False #bool(os.environ.get("DEBUG", default=0))
 
 
 
